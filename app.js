3234a9cef241792f9abda3870b20e454e5d6d95e
Viktoriia Hilenko <80678378+vixxxyg@users.noreply.github.com>
2024-10-16 00:53:47 +0200

    5th

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-window.onload = start;
+const columns = document.querySelectorAll('.column');
 
-function start(){
-    console.log(`working`);
+function generateRandomColor () {
+    const hexCodes = '0123456789ABCDEF'
+    let color = ''
+    for (let i = 0; i < 6; i++) {
+        color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
+    }
+    return '#' + color
 }
+
+function setRandomColors() {
+    columns.forEach((column)=> {
+        console.log(column);
+    })
+}
+
+setRandomColors();
