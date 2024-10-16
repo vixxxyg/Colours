f7d6db4475bfa45d9409cebbd807aae05162c04a
Viktoriia Hilenko <80678378+vixxxyg@users.noreply.github.com>
2024-10-16 17:41:13 +0200

    7th

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
-const columns = document.querySelectorAll('.column');
+const columns = document.querySelectorAll('.column')
 
-function generateRandomColor () {
-    const hexCodes = '0123456789ABCDEF'
-    let color = ''
-    for (let i = 0; i < 6; i++) {
-        color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
-    }
-    return '#' + color
-}
+// function generateRandomColor () {
+    // const hexCodes = '0123456789ABCDEF'
+    // let color = ''
+    // for (let i = 0; i < 6; i++) {
+    //     color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
+    // }
+    // return '#' + color
+// }
 
 function setRandomColors() {
     columns.forEach((column)=> {
-        const text =  column.querySelector('h2');
-        const color = generateRandomColor();
-        text.textContent = color;
-        column.style.background = color;
+        const text = column.querySelector('h2')
+        const button = column.querySelector('button')
+        const color = chroma.random()
+        // const color = generateRandomColor()
+        
+        
+        text.textContent = color
+        column.style.background = color
+
+        setColor(text, color)
+        setColor(button, color)
     })
+}
+
+function setColor(text, color) {
+    const  luminance = chroma(color).luminance()
+    text.style.color = luminance >0.5 ? 'black' : 'white'
 }
 
 setRandomColors();
